a2e1b65d301b6d94517be0a2b65b15ab290459d6
Karthkraja G <karthikrajash935@gmail.com>
2022-12-08 07:13:58 +0530

    Datatypes- Initiated to integers

---
+++ Python/Datatypes.py
@@ -0,0 +1,28 @@
+## Data types     
+
+# In python the data types are called dynamically programming language.
+
+
+
+# There are 14 types there in python
+
+# Integer ---> int
+# Floating ---> float
+# Complex ---> complex
+# string  ----> str
+# Boolean  ---> bool
+# List  ----> list
+# Tuple  ---> tuple
+# Range
+# Dictionary  ---> dict
+# Set    ---> set
+# bytes
+# bytearray        
+# None
+
+
+
+# Integer data type --> int is a datatype which is responsible for all whole numbers (eg: 0,1,2,100,13884989,5, etc)
+number= 5
+print(number)
+print(type(number))
