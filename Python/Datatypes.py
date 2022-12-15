d6a074d2e6e7684bbdbc05d014ecb0b540fd0ff8
Your Name <you@example.com>
2022-12-15 18:24:38 +0530

    Datatypes ubdated upto binary

--- Python/Datatypes.py
+++ Python/Datatypes.py
@@ -24,7 +24,15 @@
 
 # Integer data type --> int is a datatype which is responsible for all whole numbers (eg: 0,1,2,100,13884989,5, etc)
 number= 5
-print(number)
-print(type(number))
+print(number)               # Where print is the "Function/Method/Procedure" and inside bracket is called "Parameter or Arguments"
+print(type(number))         # there are many inbuilt Functions in all languages. we can Google it. 
 long_number= 38937483949000
 print(long_number)
+
+# Decimal form   ---> which is normal numbers that we are using eg: 0,1,2,3,4,.....
+
+# Binary form  --> this is 0s asd 1s  ---> we can specify this as 0b or 0B  
+no= 0b1011
+print(no)
+no1=0B1011
+print(no1)
